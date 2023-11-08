ef2a5704828e7b6c976092559ae6c0a125443dde
roczyno <adiabaroczyno9@gmail.com>
2023-11-08 15:07:57 +0000

    worked on the recent request page

--- src/Components/Pages/YourSubmittedRequests/SubmittedRequests.jsx
+++ src/Components/Pages/YourSubmittedRequests/SubmittedRequests.jsx
@@ -1,41 +1,71 @@
+import { useEffect, useState } from "react";
 import "../../../css/SubmittedRequest.css";
+import axios from "axios";
 
 const SubmittedRequests = () => {
+  const [data,setData]=useState([])
+
+  useEffect(()=>{
+const getLatestRequest=async()=>{
+try {
+  
+  const res= await axios.get("https://hackathon-waste-api.onrender.com/api/v1/waste-request/latest",{
+    headers:{
+      token:"Bearer " + JSON.parse(localStorage.getItem("user")).accessToken
+    }
+  })
+  setData(res.data)
+} catch (error) {
+  console.log(error)
+}
+}
+getLatestRequest()
+  },[])
+
   return (
     <div className="submitted_wrapper">
       <div className="inner_wrapper">
         <div className="header">
           <h4>Your recent request</h4>
         </div>
-        <div className="details">
+      
+    
+      <div className="details">
+      
+      
+
+      
+      <>
           <div className="data">
-            <h6>Name:</h6>
-            <p>Sam</p>
+            <h6>Name: </h6>
+            <p>{data.username}</p>
           </div>
           <div className="data">
             <h6>Location:</h6>
-            <p>Accra</p>
+            <p>{data.location}</p>
           </div>
           <div className="data">
             <h6>Landmark:</h6>
-            <p>Market</p>
+            <p>{data.landmark}</p>
           </div>
           <div className="data">
             <h6>Date and Time:</h6>
-            <p>01/03/2015 5:00pm</p>
+            <p>{data?.data}</p>
           </div>
           <div className="data">
             <h6>Waste Type:</h6>
-            <p>Plastic</p>
+            <p>{data.wasteType}</p>
           </div>
           <div className="data">
             <h6>Number Of Bins:</h6>
-            <p>2</p>
+            <p>{data.numberOfBins}</p>
           </div>
           <div className="data">
             <h6>Service Option:</h6>
-            <p>Regular Picker</p>
+            <p>{data.serviceOption}</p>
           </div>
+          </>
+          
         </div>
       </div>
     </div>
