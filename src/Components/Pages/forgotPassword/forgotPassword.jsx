e7db02088c49637f8ce756fcf3ff8057be28c4d1
samasare <samuellexasare@gmail.com>
2023-11-09 22:53:58 +0000

    Build -- forget password

--- src/Components/Pages/forgotPassword/forgotPassword.jsx
+++ src/Components/Pages/forgotPassword/forgotPassword.jsx
@@ -1,52 +1,65 @@
+import "../../../css/ForgetPassword.css";
+
 import { useState } from "react";
 
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const ForgotPassword = () => {
-  const [error, setError] = useState("");
-  const [msg, setMsg] = useState("");
-  const [email, setEmail] = useState();
+    const [error, setError] = useState("");
+    const [msg, setMsg] = useState("");
+    const [email, setEmail] = useState();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-    try {
-      const res = await axios.post(
-        "https://hackathon-waste-api.onrender.com/api/v1/auth/password-reset",
-        { email }
-      );
-      console.log(res.data);
-      setMsg(res.data.message);
-    } catch (error) {
-      console.log(error);
-      if (
-        error.response &&
-        error.response.status >= 400 &&
-        error.response.status <= 500
-      ) {
-        setError(error.response.data.message);
-        setMsg("");
-      }
-    }
-  };
+        try {
+            const res = await axios.post(
+                "https://hackathon-waste-api.onrender.com/api/v1/auth/password-reset",
+                { email }
+            );
+            console.log(res.data);
+            setMsg(res.data.message);
+        } catch (error) {
+            console.log(error);
+            if (
+                error.response &&
+                error.response.status >= 400 &&
+                error.response.status <= 500
+            ) {
+                setError(error.response.data.message);
+                setMsg("");
+            }
+        }
+    };
 
-  return (
-    <div className="forgot-password">
-      <form onSubmit={handleSubmit}>
-        <h1>Forgot Password</h1>
-        <input
-          type="email"
-          placeholder="enter your email for password reset"
-          className="input"
-          onChange={(e) => setEmail(e.target.value)}
-        />
+    return (
+        <div className="forget_password_wrapper ">
+            <div className="inner_wrapper">
+                <div className="header_text">
+                    <h1>Forgot Password</h1>
+                </div>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="email"
+                        placeholder="Enter your email for password reset"
+                        className="input"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <div className="alternative_links">
+                        <Link to="/signup">Sign Up</Link>
+                        <Link to="/login">Log In</Link>
+                    </div>
 
-        <button type="submit">Submit</button>
-        {error && <span style={{ color: "red" }}>{error}</span>}
-        {msg && <span style={{ color: "red" }}>{msg}</span>}
-      </form>
-    </div>
-  );
+                    <button type="submit" className="btn_submit">
+                        Submit
+                    </button>
+                    {error && <span style={{ color: "red" }}>{error}</span>}
+                    {msg && <span style={{ color: "red" }}>{msg}</span>}
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default ForgotPassword;
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -17,36 +17,47 @@ import BlogComponentsContainer from "./Components/Pages/BlogPage/BlogComponents"
 import EachBlogCard from "./Components/Pages/BlogPage/EachBlogCard/EachBlogComponent";
 
 const MyRoutes = () => {
-  const { user } = useContext(AuthContext);
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/request" element={user ? <RequestPage /> : <Login />} />
-        <Route path="/review" element={user ? <MainReviewPage /> : <Login />} />
-        <Route
-          path="/completed"
-          element={user ? <CompletedRequest /> : <Login />}
-        />
-            <Route
+    const { user } = useContext(AuthContext);
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route
+                    path="/request"
+                    element={user ? <RequestPage /> : <Login />}
+                />
+                <Route
+                    path="/review"
+                    element={user ? <MainReviewPage /> : <Login />}
+                />
+                <Route
+                    path="/completed"
+                    element={user ? <CompletedRequest /> : <Login />}
+                />
+                <Route
                     path="/blogpage"
                     element={user ? <BlogComponentsContainer /> : <Login />}
                 />
-                <Route path="/blog/:item" element={<EachBlogCard />} />
-        <Route path="api/auth/:id/verify/:token" element={<Email />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/password-reset/:id/:token" element={<PasswordReset />} />
-        <Route
-          path="/submittedrequests"
-          element={user ? <SubmittedRequests /> : <Login />}
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
-  )
-
+                <Route
+                    path="/blog/:item"
+                    element={user ? <EachBlogCard /> : <Login />}
+                />
+                <Route path="api/auth/:id/verify/:token" element={<Email />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+                <Route
+                    path="/password-reset/:id/:token"
+                    element={<PasswordReset />}
+                />
+                <Route
+                    path="/submittedrequests"
+                    element={user ? <SubmittedRequests /> : <Login />}
+                />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </div>
+    );
 };
 
 export default MyRoutes;
